687279a1973aa837d5ba1252cd06e34c3bc07111
Bektemur Mamashaev <bektemurmamashayev07@gmail.com>
2023-04-02 22:03:30 +0500

    Initial Commit

---
+++ TaskList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskList
+//
+//  Created by Bektemur Mamashayev on 02/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
